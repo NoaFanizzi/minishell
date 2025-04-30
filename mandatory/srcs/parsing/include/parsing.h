27041cd1283ebc039ca0befb3c019cb16d0675d9
nathan <nbodin@student.42lyon.fr>
2025-04-30 10:01:02 +0200

    nothing more, just figuring out our structure to join codes

--- mandatory/srcs/parsing/include/parsing.h
+++ mandatory/srcs/parsing/include/parsing.h
@@ -6,7 +6,7 @@
 /*   By: nbodin <nbodin@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 15:09:58 by nbodin            #+#    #+#             */
-/*   Updated: 2025/04/29 17:20:11 by nbodin           ###   ########lyon.fr   */
+/*   Updated: 2025/04/30 09:53:17 by nbodin           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,22 +60,28 @@ typedef struct s_env
 // int export_size;
 // }t_var;
 
-enum FILES {
-	PIPE = -2,
-	STDIN = -3,
-	STDOUT = -4,
-	STDERR = -5
-};
+enum redir
+{
+    IN,
+    OUT,
+    APND, //append
+    HDOC // sortie d'erreur
+} redir;
+
+
+typedef struct s_files
+{
+    int    		index;
+    enum redir type;
+}                t_files;
 
 typedef struct s_content //TODO toujours malloc cmd a minimum 4 parce que j'ai 4 trucs a envoyer et ca evite d'avoir a faire des reallocations
 {
-	char 	**cmd;// Peut-etre faire un tableau de tableau pour cmd + options parce que moi je dois donner un tableau de tableau a execve
-	char 	*arg; //TODO TEJ le arg et tout mettre dans cmd
-	int 	input; // int ou char ? Est ce que j'open dans l'exec ou on open dans le parsing ?
-	int 	output; //pareil pour l'output
-	int 	overwrite;
-	pid_t 	pid;
-}				t_content;
+    char 	**cmd;// Peut-etre faire un tableau de tableau pour cmd + options parce que moi je dois donner un tableau de tableau a execve
+    char	*arg; //TODO TEJ le arg et tout mettre dans cmd
+    t_files *files;
+    pid_t 	pid;
+}            t_content;
 
 typedef struct s_array
 {
