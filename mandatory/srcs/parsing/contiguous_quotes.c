7ed7d9737a07cf7afb294fbb4b8463075c24ba39
nathan <nbodin@student.42lyon.fr>
2025-05-12 10:58:23 +0200

    nothing new

--- mandatory/srcs/parsing/contiguous_quotes.c
+++ mandatory/srcs/parsing/contiguous_quotes.c
@@ -6,7 +6,7 @@
 /*   By: nbodin <nbodin@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 17:06:12 by nbodin            #+#    #+#             */
-/*   Updated: 2025/05/09 09:56:51 by nbodin           ###   ########lyon.fr   */
+/*   Updated: 2025/05/12 10:28:45 by nbodin           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,58 +19,67 @@ int		ft_isspace(char c)
 	return (0);
 }
 
-void	fusion_quotes(char **command, char **joined, size_t i, size_t j)
+void	fusion_quotes_next(char **command, char **joined, size_t i, size_t j)
 {
 	size_t	size;
 	
 	rem_and_shift(command[j]);
 	rem_and_shift(command[i]);
 	size = ft_strlen(command[j]) + ft_strlen(command[i]) + 3;
-	joined[j] = malloc(size * sizeof(char));
-	if (!joined[j])
+	joined[i] = malloc(size * sizeof(char));
+	if (!joined[i])
 		return ;
-	joined[j][0] = D_QUOTE;
-	joined[j][1] = 0;
-	strlcat(joined[j], command[j], sizeof(joined[j]));
-	strlcat(joined[j], command[j], sizeof(joined[j]));
+	joined[i][0] = D_QUOTE;
+	joined[i][1] = 0;
+	strlcat(joined[i], command[i], sizeof(joined[i]));
+	strlcat(joined[i], command[j], sizeof(joined[i]));
 	joined[size - 2] = D_QUOTE;
 	joined[size - 1] = 0;
 }
 
-char	**join_previous_quotes(char **command, size_t i)
+void	go_through_join_next_quotes(char **command, char **joined, size_t i)
 {
 	size_t	j;
-	char **joined;
 
-	j = 0;
-	while (command[j])
-		j++;
-	joined = malloc(j * sizeof(char *));
-	if (!joined)
-		return (NULL);
 	j = 0;
 	while (command[j])
 	{
-		if (j + 1 == i)
-		{
+		if (i + 1 == j)
 			fusion_quotes(command, joined, i, j);
-			if (!joined[j])
-				return (NULL);
-			j++;
-		}
 		else
-		{
 			joined[j] = ft_strdup(command[j]);
-			if (!joined[j])
-				return (NULL);
+		if (!joined[j])
+		{
+			//free command
+			return ;
 		}
 		j++;
 	}
+}
+
+        i
+hello "hey""rahh" salut
+        j
+
+char	**join_next_quotes(char **command, size_t i)
+{
+	size_t	size;
+	char **joined;
+
+	size = 0;
+	while (command[size])
+		size++;
+	joined = malloc(size * sizeof(char *));
+	if (!joined)
+	{
+		//free command
+		return (NULL);
+	}
+	go_through_join_next_quotes(command, joined, i);
+	if ()
 	//free command
 	return (joined);
 }
-
-"hello""test" hey
 
 void	join_previous_simple(char **command, size_t i);
 void	join_next_quotes(char **command, size_t i);
@@ -88,16 +97,16 @@ char	**contigous_quotes(char **command)
 	{
 		if (command[i][0] == D_QUOTE || command[i][0] == S_QUOTE)
 		{
-			if (i > 0 && (command[i - 1][ft_strlen(command[i - 1]) - 1] == D_QUOTE
-				|| command[i - 1][ft_strlen(command[i - 1]) - 1] == S_QUOTE))
-				command = join_previous_quotes(command, i);
-			else if (i > 0 && ft_isspace(command[i - 1][ft_strlen(command[i - 1]) - 1]) == 0)
+			// if (i > 0 && (command[i - 1][ft_strlen(command[i - 1]) - 1] == D_QUOTE
+			// 	|| command[i - 1][ft_strlen(command[i - 1]) - 1] == S_QUOTE))
+			// 	command = join_previous_quotes(command, i);
+			if (i > 0 && ft_isspace(command[i - 1][ft_strlen(command[i - 1]) - 1]) == 0)
 				//join with the previous
 		}
 		if (command[i][0] == D_QUOTE || command[i][0] == S_QUOTE)
 		{
 			if (command[i + 1] && (command[i + 1] == D_QUOTE || command[i + 1] == S_QUOTE))
-				//join with other quotes
+				command = join_next_quotes(command, i);
 			else if (command[i + 1] && ft_isspace(command[i + 1][0]) == 0)
 				//join with the next
 		}
