f52abe7ad528e0c1bb1d3b6235aa68935b3961d5
NOAALTF4 <noafanizzistdy@gmail.com>
2025-07-02 12:41:37 +0200

    reorganized the get_redir_dad function

--- mandatory/srcs/exec/parents/parents_get_redirections.c
+++ mandatory/srcs/exec/parents/parents_get_redirections.c
@@ -6,11 +6,24 @@
 /*   By: nofanizz <nofanizz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 16:57:15 by nofanizz          #+#    #+#             */
-/*   Updated: 2025/07/02 12:33:46 by nofanizz         ###   ########.fr       */
+/*   Updated: 2025/07/02 12:40:00 by nofanizz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	exit_in_parent(t_array *array)
+{
+	if(array->content->cmd && ft_strcmp(array->content[0].cmd[0], "exit") == 0)
+	{
+		if(array->content[0].arg)
+			array->content[0].error_code = ft_atoi(array->content->arg[0]);
+		ft_putstr_fd("exit\n", STDERR_FILENO);
+		ft_exit(&array->content[0]);
+		return(2);
+	}
+	return(1);
+}
 
 int	ft_get_redir_dad(t_array *array, t_list **env)
 {
@@ -27,25 +40,11 @@ int	ft_get_redir_dad(t_array *array, t_list **env)
 		ft_is_built_in_dad(array, env);
 		array->p_exit_status = array->content[0].error_code;
 	}
-	if(command == 1)
-	{
-		dup2(stdin_saved, STDIN_FILENO);
-		close(stdin_saved);
-		dup2(stdout_saved, STDOUT_FILENO);
-		close(stdout_saved);
-		if(array->content->cmd && ft_strcmp(array->content[0].cmd[0], "exit") == 0)
-		{
-			if(array->content[0].arg)
-				array->content[0].error_code = ft_atoi(array->content->arg[0]);
-			ft_putstr_fd("exit\n", STDERR_FILENO);
-			ft_exit(&array->content[0]);
-			return(2);
-		}
-		return(1);
-	}
 	dup2(stdin_saved, STDIN_FILENO);
 	close(stdin_saved);
 	dup2(stdout_saved, STDOUT_FILENO);
 	close(stdout_saved);
+	if(command == 1)
+		return(exit_in_parent(array));
 	return(0);
 }
