bdb407b6d150260e3c88e96a78b51c5e270bdb17
n12code <nathanbodin006@gmail.comm>
2025-07-19 17:48:26 +0200

    doing expand leaks

--- .dep/get_next_line.d
+++
@@ -1,6 +0,0 @@
-.obj/get_next_line.o: mandatory/srcs/GNL/get_next_line.c \
-  mandatory/includes/get_next_line.h libft/includes/libft.h
-
-mandatory/includes/get_next_line.h:
-
-libft/includes/libft.h:
--- .dep/get_next_line_utils.d
+++
@@ -1,6 +0,0 @@
-.obj/get_next_line_utils.o: mandatory/srcs/GNL/get_next_line_utils.c \
-  mandatory/includes/get_next_line.h libft/includes/libft.h
-
-mandatory/includes/get_next_line.h:
-
-libft/includes/libft.h:
